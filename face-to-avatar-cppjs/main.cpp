f2b6d037e4b7737327b9576992746fa5edf7fd02
XarthDaichi <diegoqahh@gmail.com>
2022-05-03 16:33:47 -0600

    trying to fix submodule error

---
+++ face-to-avatar-cppjs/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <GL/freeglut.h>
+#include <GL/glew.h>
+
+using namespace std;
+
+GLuint VBO;
+
+static void RenderSceneCB() {
+
+}
+
+int main(int argc, char** argv) {
+	return 0;
+}
